203b387079e326b4a8086c5f842835f2922f337e
Youcef <ybena102@uottawa.ca>
2021-06-10 14:03:48 -0400

    CommitSet1 - Passing Test

--- lab04/fizzbuzz_java/test/FizzbuzzTest.java
+++ lab04/fizzbuzz_java/test/FizzbuzzTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 public class FizzbuzzTest {
     @Test
     public void nextDate_sample() {
-        assertNull(Fizzbuzz.FizzBuzzConverter(null));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Fizzbuzz.FizzBuzzConverter(null);
+        });
     }
 }
